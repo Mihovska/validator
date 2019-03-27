14128cf9989df395349b075060da4987e0c8c16e
Rafael Gil <rapha.leon@gmail.com>
2019-03-27 10:31:13 -0300

    "adding context description to tests"

--- validator/test/validator.spec.js
+++ validator/test/validator.spec.js
@@ -11,45 +11,46 @@ function expectedToIncludeErrorWhenInvalid(number, error) {
   });
 };
 
-describe('A Validator', () => {
+describe('A Validation', () => {
   let validator;
 
-  beforeEach(() => {
-    validator = validatorWith([
-      nonPositiveValidationRule,
-      nonDivisibleValidationRule(3, 'error.three'),
-      nonDivisibleValidationRule(5, 'error.five')
-    ]);
-  });
+  context('using the default validation rules:', () => {
+    beforeEach(() => {
+      validator = validatorWith([
+        nonPositiveValidationRule,
+        nonDivisibleValidationRule(3, 'error.three'),
+        nonDivisibleValidationRule(5, 'error.five')
+      ]);
+    });
 
-  it('will return no errors for valid numbers', () => {
-    expect(validator(1)).to.be.empty;
-  });
+    it('will return no errors for valid numbers', () => {
+      expect(validator(1)).to.be.empty;
+    });
 
-  describe('will include error.nonpositive for not strictly positive numbers', () => {
-    it('like 0', () => {
-      expect(validator(0)).to.be.include('error.nonpositive');
+    context('for not strictly positive numbers:', () => {
+      it('like 0, will include error.nonpositive', () => {
+        expect(validator(0)).to.be.include('error.nonpositive');
+      });
+      it('like -2, will include error.nonpositive', () => {
+        expect(validator(-2)).to.be.include('error.nonpositive');
+      });
     });
-    it('like -2', () => {
-      expect(validator(-2)).to.be.include('error.nonpositive');
-    });
-  });
 
-  describe('will include error.three for divisible y 3 numbers:', () => {
-    it('like 3', () => {
-      expect(validator(3)).to.be.include('error.three');
+    context('for numbers divisible by 3', () => {
+      it('like 3, will include error.three', () => {
+        expect(validator(3)).to.be.include('error.three');
+      });
+      it('like 15, will include error.three', () => {
+        expect(validator(15)).to.be.include('error.three');
+      });
     });
-    it('like 15', () => {
-      expect(validator(15)).to.be.include('error.three');
-    });
-  });
-
-  describe('will include error.five for divisible by 5 numbers:', () => {
-    it('like 5', () => {
-      expect(validator(5)).to.be.include('error.five');
-    });
-    it('like 15', () => {
-      expect(validator(15)).to.be.include('error.five');
+    context('for numbers divisible by 5:', () => {
+      it('like 5, will include error.five', () => {
+        expect(validator(5)).to.be.include('error.five');
+      });
+      it('like 15, will include error.five', () => {
+        expect(validator(15)).to.be.include('error.five');
+      });
     });
   });
 });
